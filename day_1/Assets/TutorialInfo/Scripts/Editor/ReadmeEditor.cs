3c91fbbe1087e9f8a00deda0dc3295db8a770be3
Mahmoud Mamdouh <Mahmoudmamdouhm3@gmail.com>
2025-12-29 23:07:33 +0200

    day1_tasks

---
+++ day_1/Assets/TutorialInfo/Scripts/Editor/ReadmeEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RiderTest : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 50f;
+
+    void Start()
+    {
+        // If this appears in the Unity Console, the connection is working!
+        Debug.Log("<color=green>Rider is successfully connected to Unity!</color>");
+    }
+
+    void Update()
+    {
+        // This will rotate the object so you can see the script is active
+        transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
+    }
+}
